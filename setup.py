1c85ad31a2ca54b8b44f1e8fa3922293ddf9adf1
Samyak S Sarnayak <samyak201@gmail.com>
2020-09-06 20:42:36 +0530

    [CODE] add project structure, setup.py, requirements.txt

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyheiko", # Replace with your own username
+    version="0.0.1",
+    author="Pranav Kesavarapu",
+    author_email="pranavkesavarapu@gmail.com",
+    description="A fancy load balancer for light weight devices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/psiayn/heiko",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
