8ea9c81e5c101d27bfd05bdb5f9d102cbee565a6
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2020-09-06 22:49:56 +0530

    Added yaml config to store details

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 heiko is a lightweight load balancer that uses ssh to manage servers running on low-end hardware such as raspberry pis or even phones
 
 heiko stands for "equilibrium" in japanese
+
+create a config.yaml file as .config/config.yaml
+a sample config file is given in sample-config.yaml
+
--- temp/something.py
+++ temp/something.py
@@ -1,10 +1,28 @@
 import paramiko
+import yaml
 
-client = paramiko.SSHClient()
-client.load_system_host_keys()
-client.connect('', username='', password='', port=8022)
-command_list = ["cd JoJo-Telegram-Bot", "source env/bin/activate", "python reply.py"]
-stdin, stdout, stderr = client.exec_command("; ".join(command_list))
-for line in stdout:
-    print('... ' + line.strip('\n'))
-client.close()
+stream = open("../.config/config.yaml")
+config = yaml.load(stream, Loader=yaml.CLoader)
+
+host = config['nodes'][0]['host']
+username = config['nodes'][0]['username']
+port = config['nodes'][0]['port']
+password = config['nodes'][0]['password']
+
+try:
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.connect(host, username=username, password=password, port=port)
+
+    command_list = ["cd JoJo-Telegram-Bot", "python3 reply.py"]
+    stdin, stdout, stderr = client.exec_command("; ".join(command_list))
+    for line in stdout:
+        print('... ' + line.strip('\n'))
+
+except Exception as e:
+    print("closing connection")
+    client.close()
+
+finally:
+    print("closing connection")
+    client.close()
