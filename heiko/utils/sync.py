57d7ddf2fc203845427c75d40157866ce3d2d87f
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2021-01-09 18:40:10 +0530

    rsync over ssh

--- heiko/utils/sync.py
+++ heiko/utils/sync.py
@@ -6,9 +6,9 @@ def sync_folder(node: Node):
     pwd = os.getcwd()
     if node.password:
         subprocess.run(["sshpass", "-p", node.password, 
-                "rsync", "-a", "-v", "-e", f"ssh -p {node.port}","--exclude-from=rsync-ignore",
+                "rsync", "-a", "-e", f"ssh -p {node.port}", "-v", "--exclude-from=rsync-ignore",
                 pwd, f"{node.username}@{node.host}:~/.heiko/"])
     else:
-        subprocess.run(["rsync", "-a", "-v", "--exclude-from=rsync-ignore",
+        subprocess.run(["rsync", "-a", "-e", f"ssh -p {node.port}", "-v", "--exclude-from=rsync-ignore",
                 pwd, f"{node.username}@{node.host}:~/.heiko/"])
 
