96a9d388ce244318e4f0d66229c9ad0bc92a50de
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2021-01-17 14:06:27 +0530

    made docker-networks more interactive

--- docker-networks.py
+++ docker-networks.py
@@ -1,10 +1,57 @@
 import json
 import atexit
 import subprocess
-import sys
 import yaml
+import argparse
+import textwrap
+from dataclasses import dataclass
 
-command = ["cd ~/Downloads", "touch sample.txt"]
+@dataclass
+class Config:
+    num_nodes: int
+    config_path: str
+    def __init__(self, n: int, c: str):
+        self.num_nodes = n
+        self.config_path = c
+        self.place_holder_commands = ["cd ~/Downloads", "touch sample.txt"]
+
+def make_parser():
+    parser_ = argparse.ArgumentParser(
+        prog="heiko-docker-test",
+        description=textwrap.dedent(
+            """
+            heiko-docker-test allows you test your heiko config
+            locally with just docker.\n
+
+            It takes 2 arguments, number of nodes (containers) and
+            config_path (the path where the config is written to).\n
+
+            Using the provided args, it generates a config file which
+            connects to the containers. The config can then be further 
+            modified to provide the required jobs to be run.\n
+
+            After modifying the config, deploy heiko to test.
+            """
+        ),
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser_.add_argument(
+        "-n",
+        "--number",
+        help="Number of nodes",
+        required=True,
+        action="store",
+        dest="number_of_nodes",
+    )
+    parser_.add_argument(
+        "-c",
+        "--config_path",
+        help="Path where the config should be generated",
+        required=True,
+        action="store",
+        dest="config",
+    )
+    return parser_
 
 def genYAML(path, nodes, command):
     stream = open(path, 'w')
@@ -13,15 +60,18 @@ def genYAML(path, nodes, command):
     print(yaml.dump(config, ))
     stream.close()
 
-
-num_nodes = sys.argv[1]
-config_path = sys.argv[2]
-lolcommand = sys.argv[3]
-num_nodes = int(num_nodes)
+parser = make_parser()
+args = parser.parse_args()
+n = int(args.number_of_nodes)
+c = args.config
+config = Config(n, c)
+# num_nodes = sys.argv[1]
+# config_path = sys.argv[2]
+# num_nodes = int(num_nodes)
 name = "heiko_node"
 nodes = list()
 print("Starting containers ..........")
-for i in range(num_nodes):
+for i in range(config.num_nodes):
     nodes.append(dict())
     node_name = name + str(i)
     # spawn containers
@@ -38,7 +88,7 @@ network = json.loads(out)
 # print(network)
 # print(network[0]['Containers'])
 
-for i in range(num_nodes):
+for i in range(config.num_nodes):
     node_name = name + str(i)
     cid = subprocess.check_output(['docker', "ps", "-a", "-q", "--no-trunc", "--filter", f'name={node_name}'])
     # print(cid)
@@ -47,19 +97,21 @@ for i in range(num_nodes):
 
 print()
 print("YAML CONFIG")
-genYAML(config_path, nodes, command)
-    
+genYAML(config.config_path, nodes, config.place_holder_commands)
+
+
 def cleanup():
     print("Stopping containers .........")
-    for i in range(num_nodes):
+    for i in range(config.num_nodes):
         node_name = name + str(i)
         p = subprocess.Popen(['docker', 'stop', node_name])
         p.wait()
     print("Removing containeres ............")
-    for i in range(num_nodes):
+    for i in range(config.num_nodes):
         node_name = name + str(i)
         p = subprocess.Popen(['docker', 'rm', node_name])
         p.wait()
+
 
 atexit.register(cleanup)
 input("Present enter to stop workers")
